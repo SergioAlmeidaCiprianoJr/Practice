376be2f43abe27eaa2470f0b942c4d66099e98e9
Sergio Cipriano <sergiosacj@hotmail.com.br>
2019-12-20 19:59:09 -0300

    Balloon++

---
+++ Competitive Programming/URI/1065.py
@@ -0,0 +1,7 @@
+x = 5
+ans = 0
+for y in range(0,5):
+	y = int(input())
+	if(y%2==0):
+		ans+=1
+print(str(ans) + " valores pares")
---
+++ Competitive Programming/URI/1157.py
@@ -0,0 +1,5 @@
+x = int(input());
+print('1')
+for y in range(2,x+1):
+	if(x%y==0):
+		print(y)
